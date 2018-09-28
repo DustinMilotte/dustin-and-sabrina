19f2a2f41d56f21d4292ed15d5cb7e388555a458
Dustin Milotte <dmilotte@gmail.com>
2018-09-28 19:37:50 -0400

    changed pedestal, add dirt and stone

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-var path = require('path');
+var path = require("path");
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
-app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/garden-scene.html')));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname + "/garden-scene.html"))
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
